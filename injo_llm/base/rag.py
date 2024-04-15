3d27f3404ae37e5c5b3aebef142455d5570091db
Kiminjo <deulseong1121@gmail.com>
2024-04-15 10:46:16 +0900

    🐛 Fix indent of rag function

--- injo_llm/base/rag.py
+++ injo_llm/base/rag.py
@@ -23,39 +23,39 @@ class RAG:
             self.llm_model = self.set_llm_model(llm_model)
     
     def fit(self, documents: Union[str, List[str]], db_path: Union[str, Path] = "db/rag.index", document_path: Union[str, Path] = "db/documents.pkl"):
-            """
-            Train the RAG model
-            
-            Args:
-                - documents: str or List[str]
-                    The documents for the RAG model
-                - db_path: str or Path
-                    The path to save the database
-                - document_path: str or Path
-                    The path to save the document
-            """
-            # Convert str type of document to Document type
-            if isinstance(documents, str):
-                documents = [documents]
-            if isinstance(db_path, str):
-                db_path = Path(db_path)
-            if isinstance(document_path, str):
-                document_path = Path(document_path)
-            
-            # Make save dir 
-            db_path.parent.mkdir(parents=True, exist_ok=True)
-            document_path.parent.mkdir(parents=True, exist_ok=True)
+        """
+        Train the RAG model
+        
+        Args:
+            - documents: str or List[str]
+                The documents for the RAG model
+            - db_path: str or Path
+                The path to save the database
+            - document_path: str or Path
+                The path to save the document
+        """
+        # Convert str type of document to Document type
+        if isinstance(documents, str):
+            documents = [documents]
+        if isinstance(db_path, str):
+            db_path = Path(db_path)
+        if isinstance(document_path, str):
+            document_path = Path(document_path)
+        
+        # Make save dir 
+        db_path.parent.mkdir(parents=True, exist_ok=True)
+        document_path.parent.mkdir(parents=True, exist_ok=True)
 
-            # Set embeddings and DB
-            embedding_vectors = self.llm_model.embedding(documents)
-            faiss_index = faiss.IndexFlatL2(embedding_vectors.shape[1])
-            faiss_index.add(embedding_vectors)
-            
-            # Save the vector store and document as pickle
-            faiss.write_index(faiss_index, str(db_path))
-            with open(document_path, "wb") as f:
-                pickle.dump(documents, f)
-                f.close()
+        # Set embeddings and DB
+        embedding_vectors = self.llm_model.embedding(documents)
+        faiss_index = faiss.IndexFlatL2(embedding_vectors.shape[1])
+        faiss_index.add(embedding_vectors)
+        
+        # Save the vector store and document as pickle
+        faiss.write_index(faiss_index, str(db_path))
+        with open(document_path, "wb") as f:
+            pickle.dump(documents, f)
+            f.close()
     
     def set_llm_model(self, llm_model: BaseOpenAILLM):
         """
@@ -67,38 +67,38 @@ class RAG:
         self.llm_model = llm_model
 
     def search(self, query: str, db_path: Union[str, Path] = "db/rag.index", document_path: Union[str, Path] = "db/documents.pkl", top_k: int = 5):
-            """
-            Search the RAG model
-            
-            Args:
-                - query: str
-                    The query for the search
-                - db_path: str or Path
-                    The path to save the database
-                - document_path: str or Path
-                    The path to save the document
-                - top_k: int
-                    The number of top-k results to return
-            
-            Returns:
-                - results: List[str]
-                    The top-k results from the search
-            """
-            # Load the DB and documents
-            faiss_index = faiss.read_index(str(db_path))
-            with open(document_path, "rb") as f:
-                documents = pickle.load(f)
-                f.close()
+        """
+        Search the RAG model
+        
+        Args:
+            - query: str
+                The query for the search
+            - db_path: str or Path
+                The path to save the database
+            - document_path: str or Path
+                The path to save the document
+            - top_k: int
+                The number of top-k results to return
+        
+        Returns:
+            - results: List[str]
+                The top-k results from the search
+        """
+        # Load the DB and documents
+        faiss_index = faiss.read_index(str(db_path))
+        with open(document_path, "rb") as f:
+            documents = pickle.load(f)
+            f.close()
 
-            # Get the embedding from the query
-            query_embedding = self.embedding(query)
-            query_embedding = query_embedding[0]
+        # Get the embedding from the query
+        query_embedding = self.embedding(query)
+        query_embedding = query_embedding[0]
 
-            # Search the DB
-            _, I = faiss_index.search(np.array([query_embedding]), top_k)
-            results = [documents[i] for i in I[0] if i != -1]
-            return results
-    
+        # Search the DB
+        _, I = faiss_index.search(np.array([query_embedding]), top_k)
+        results = [documents[i] for i in I[0] if i != -1]
+        return results
+
     def generate_simple(self, prompt: str):
         """
         Generate the answer based on RAGed information 
@@ -117,6 +117,6 @@ class RAG:
         answer = self.llm_model.generate(prompt)
         return answer
     
-    
+
          
 
