edd2d3d04111723a4d8dbebbfca3a47db571c40a
Kiminjo <deulseong1121@gmail.com>
2024-04-16 23:35:33 +0900

    Style: 🎨 Add abstract class for base code

--- injo_llm/__init__.py
+++ injo_llm/__init__.py
@@ -1,4 +1,4 @@
 from .langchain.base import LangchainBaseLLM
-from .base.openai import BaseOpenAILLM
+from .base.openai import OpenAILLM
 from .base.azure import BaseAzureLLM
 from .base.rag import RAG
