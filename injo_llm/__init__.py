06990cb4a438024932248e6a47af07f6b2417f8d
Kiminjo <deulseong1121@gmail.com>
2024-04-28 10:28:56 +0900

    [Refactor] ♻️ Add base search prompt and other refactoring

--- injo_llm/__init__.py
+++ injo_llm/__init__.py
@@ -1,4 +1,4 @@
-from .langchain.base import LangchainBaseLLM
+from .models.langchain import LangchainBaseLLM
 from .models.models import OpenAILLM, AzureLLM
 from .models.rag import RAG
 from .models.factory import ModelFactory
--- injo_llm/models/rag.py
+++ injo_llm/models/rag.py
@@ -15,10 +15,13 @@ sys.path.append(str(Path(__file__).parents[2]))
 
 # Custom Libraries
 from injo_llm import OpenAILLM, AzureLLM
-from injo_llm.prompts.retrieval import retrieval_base_prompt
+from injo_llm.prompts.library import retrieval_prompt
 
 class RAG:
-    def __init__(self, llm_model: Union[OpenAILLM, AzureLLM] = None, openai_api_key: str = None):
+    def __init__(self, 
+                 llm_model: Union[OpenAILLM, AzureLLM] = None, 
+                 openai_api_key: str = None
+                 ):
         if llm_model is not None:
             self.set_llm_model(llm_model)
         else:
@@ -133,7 +136,7 @@ class RAG:
         related_doc = self.search(prompt)
         
         # Set the prompt
-        self.llm_model.set_system_prompt(system_prompt=retrieval_base_prompt, additional_info={"info": related_doc})
+        self.llm_model.set_system_prompt(system_prompt=retrieval_prompt, additional_info={"info": related_doc})
         
         # Generate the answer
         answer = self.llm_model.generate(prompt)
