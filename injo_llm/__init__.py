a17a993e579dbb2207a04d96b4979079f3fba650
Kiminjo <deulseong1121@gmail.com>
2024-05-17 15:09:21 +0900

    [Fix] 🐛 Add BaseLLM module in init function

--- injo_llm/__init__.py
+++ injo_llm/__init__.py
@@ -1,4 +1,4 @@
 from .models.langchain import LangchainBaseLLM
-from .models.models import OpenAILLM, AzureLLM, GroqLLM, LMStudioLLM
+from .models.models import BaseLLM, OpenAILLM, AzureLLM, GroqLLM, LMStudioLLM
 from .models.rag import RAG
 from .models.factory import ModelFactory
