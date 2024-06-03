c29c4c92a57451e8d1edf7b853f535fcb5635f9d
Kiminjo <deulseong1121@gmail.com>
2024-06-03 22:22:00 +0900

    ✅ [Test] Add embedding test code for client

--- server/client.py
+++ server/client.py
@@ -1,15 +1,44 @@
 import requests
 import os 
 
-url = "http://localhost:8000/api/v1/generate"
-api_key = os.environ.get("OPENAI_API_KEY")
+llm_type = "openai"
 
-chat_input = {
-    "api_key": api_key,
-    "model_type": "openai",
-    "model_name": "gpt-3.5-turbo-1106",
-    "prompt": "who is the president of the united states?"
-}
+url = f"http://localhost:8000/api/v1/{llm_type}/generate"
 
-response = requests.post(url, json=chat_input)
-print(response.json())
+if llm_type == "openai":
+    api_key = os.environ.get("OPENAI_API_KEY")
+    chat_input = {
+        "api_key": api_key,
+        "llm_name": "gpt-3.5-turbo-1106",
+        "prompt": "who is the president of the united states?"
+    }
+
+elif llm_type == "groq":
+    api_key = os.environ.get("GROQ_API_KEY")
+    chat_input = {
+        "api_key": api_key,
+        "llm_name": "llama3-70b-8192",
+        "prompt": "who is the president of the united states?"
+    }
+
+ai_response = requests.post(url, json=chat_input)
+
+print("AI Response: ")
+print(ai_response.json())
+print("\n")
+
+if llm_type == "openai":
+    url = f"http://localhost:8000/api/v1/{llm_type}/embedding"
+    embedding_input = {
+        "api_key": api_key,
+        "llm_name": "text-embedding-3-small",
+        "prompt": "who is the president of the united states?"
+    }
+    
+    embedding_response = requests.post(url, json=embedding_input)
+    
+    print("Embedding Response: ")
+    print(embedding_response.json())
+    print("\n")
+    
+    
